2e50e6265b968da00dcdad0cbfa0bd60d40100f5
PravallikaVurity <pravallikavurity@gmail.com>
2026-05-01 22:21:17 +0530

    main

---
+++ JAVASCRIPT/external.js
@@ -0,0 +1,54 @@
+num="java:${10+20}"
+console.log(num);
+num='javascript ${10+20}'
+console.log(num);
+num=`addition of 10 and 20 :${10+20}`
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+num="java"
+num=50;
+console.log(num);
+
+num=true
+console.log(num);
+class sampleexample{
+    display(){
+        console.log("this is non-static display method in sampleExample");
+    }
+    test(){
+        console.log("test method");
+    }
+}
+
+function addition(){
+    console.log(10+20);
+}
+
+ s1=new sampleexample()
+ s1.display()
+ s1.test()
+
+ num=331
+ result=num%2==0?"Even Number":"Odd Number"
+ console.log(result);
+
+ a=10;
+ b='10';
+ c=null
+ d=Symbol(10);
+ e=15678903467678n
+ let f;
+ console.log("Type of variable a is:",typeof a);
+ console.log("Type of variable b is:",typeof b);
+ console.log("Type of variable c is:",typeof c);
+ console.log("Type of variable d is:",typeof d);
+ console.log("Type of variable e is:",typeof e);
